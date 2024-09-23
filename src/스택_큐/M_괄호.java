daffc47c653280d95e65b6196168eae9b9f57bde
bluewow <dogseen@gmail.com>
2024-09-23 12:32:07 +0900

    백준 https://www.acmicpc.net/problem/9012) - Stack 을 이용하여 괄호를 체크 / O(N^2)

--- src/스택_큐/M_괄호.java
+++ src/스택_큐/M_괄호.java
@@ -1,5 +1,10 @@
 package 스택_큐;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 /*
  문제 Link
  - https://www.acmicpc.net/problem/9012
@@ -10,14 +15,45 @@ package 스택_큐;
  - ()() 접합시킨 문자열도 OK
 
  시간 복잡도 계산
- -
+ - O(N^2)
 
  분석과정 & 선택한 자료구조/알고리즘
- -
+ - 페어한 경우 pop 실행
+ - pop 할 대상이 없는 경우 잘못됨 출력
 
  참고자료
  -
 
  */
 public class M_괄호 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        Stack<Character> input = new Stack<>();
+
+        for (int i = 0; i < count; i++) {
+
+            // 입력값 확인
+            String str = br.readLine();
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') {
+                    input.push('(');
+                } else {
+                    if (input.empty()) {
+                        break;
+                    } else {
+                        input.pop();
+                    }
+                }
+            }
+
+            if (input.isEmpty())
+                System.out.println("YES");
+            else {
+                System.out.println("NO");
+                input.clear();
+            }
+        }
+
+    }
 }
